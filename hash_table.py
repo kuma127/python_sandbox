3ddebc6de1d5588626a79e0065b1b2212749e36e
kumappp <kumagai27207@gmail.com>
2023-01-14 01:09:42 +0900

    ハッシュキーのコード作成

---
+++ hash_table.py
@@ -0,0 +1,34 @@
+import re
+
+
+def char_count(a_string: str)-> dict:
+    a_dict = {}
+    for char in a_string:
+        if char in a_dict:
+            a_dict[char] += 1
+        else:
+            a_dict[char] = 1
+    return a_dict
+
+
+## challenge
+def duplicate_delete(sentence: str):
+    word_list = re.split('[ .]', sentence)
+    a_dict = {}
+    out_list = []
+    for index, a_string in enumerate(word_list):
+        if not a_string in a_dict:
+            out_list.append(a_string)
+            a_dict[a_string] = index
+
+    return ' '.join(out_list).strip() + '.'
+
+
+def main():
+    a_string = 'I am a self-taught programmer looking for a job as a programmer.'
+    out = duplicate_delete(a_string)
+    print(out)
+
+
+if __name__ == "__main__":
+    main()
